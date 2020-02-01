5ebebe85f4fd2a81684cf413a93cc1c12637dd59
STR <saturasstotenkopf@gmail.com>
2020-02-01 23:07:22 +0700

    Tugas 2 React JS
    Mengumpulkan Tugas 2 React JS

---
+++ tugas_2_react_js/src/Page/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <small>
+        <b>Tugas 2 React JS</b>
+      </small>
+    </div>
+  );
+}
+export default Footer;
